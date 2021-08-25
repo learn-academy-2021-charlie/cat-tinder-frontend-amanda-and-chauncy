e17b310a711d8b998e247cf5559c484e051ffbec
Aferg15 <amanda@afergdesign.com>
2021-08-25 10:57:32 -0700

    added create functionality

--- src/mockCharacters.js
+++ src/mockCharacters.js
@@ -1,9 +1,27 @@
 
 
 let characters = [
-    {id: 1, name: 'Raymond', type: 'cat', enjoys: 'sipping tea', personality: 'smug'},
-    {id: 2, name: 'Harv', type: 'doggo', enjoys: 'a nice bone', personality: 'hippie'},
-    {id: 3, name: 'Eloise', type: 'elephant', enjoys: 'peanuts', personality: 'snooty'}
+    {
+        id: 1, 
+        name: 'Raymond', 
+        type: 'cat', 
+        enjoys: 'sipping tea', 
+        personality: 'smug'
+    },
+    {
+        id: 2, 
+        name: 'Harv', 
+        type: 'dog', 
+        enjoys: 'photography', 
+        personality: 'hippie'
+    },
+    {
+        id: 3, 
+        name: 'Eloise', 
+        type: 'elephant', 
+        enjoys: 'peanuts', 
+        personality: 'snooty'
+    }
 ]
 
 export default characters;
--- src/pages/AnimalNew.js
+++ src/pages/AnimalNew.js
@@ -1,10 +1,67 @@
 import React, {Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
 
 class AnimalNew extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      form: {
+        name: "",
+        age: "",
+        enjoys: "",
+        personality: "",
+      },
+      submitted: false
+    }
+  }
+
+  handleChange = (e) => {
+    let { form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({form: form})
+  }
+
+  handleSubmit = () => {
+    this.props.createCharacter(this.state.form)
+    this.setState({submitted: true})
+  }
+
     render() {
       return (
         <>
             <h2>This is Animal New</h2>
+            <Form>
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <Input type="text" name="name" 
+                onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="type">Age</Label>
+                <Input type="text" name="type"
+                onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="enjoys">Enjoys</Label>
+                <Input type="text" name="enjoys"
+                onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="personality">Personality</Label>
+                <Input type="text" name="personality"
+                onChange={this.handleChange}
+                />
+              </FormGroup>
+              
+              <Button name="submit" onClick={this.handleSubmit}>
+                Add New Villager
+              </Button>
+            </Form>
+            {this.state.submitted && <Redirect to="/animalindex" />}
         </>
       )
     }
